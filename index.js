9cea4d674221083206defc6b22a379f242d45f92
Jordan Stoddard <jjsincorporated@gmail.com>
2018-11-06 12:30:37 -0800

    Added post, delete, put functionality.

--- index.js
+++ index.js
@@ -7,6 +7,9 @@ const db = require('./data/db.js')
 
 const server = express();
 
+// middleware
+server.use(express.json()) // Teaches express how to parse the JSON request body
+
 server.get('/', (req, res) => {
     res.json('alive')
 })
@@ -42,7 +45,64 @@ server.get('/api/users/:id', (req, res) => {
     })
 })
 
-server.listen(9000, () => console.log('the server is alive!'))
+server.post('/api/users', async (req, res) => {
+    try {
+        const userData = req.body
+        const userId = await db.insert(userData)
+        const user = await db.findById(userId.id)
+        res.status(201).json(user)
+    } catch (error) {
+        res.status(500).json({message: 'error creating user', error})
+    }
+})
+
+// error !== exception
+
+// server.post('/api/users', (req, res) => {
+//     const userData = req.body
+//         db.insert(req.body)
+//         .then(userId => {
+//             res.status(201).json(userId)
+//         }).catch(error => {
+//         res.status(500).json({message: 'error creating user', error})
+
+
+server.delete('/api/users/:id', (req, res) => {
+    db.remove(req.params.id)
+    .then(count => {
+        res.status(200).json(count)
+    }).catch(err => {
+        res.status(500).json({message: 'error deleting user'})
+    })
+})
+
+server.put('api/users/:id', (req, res) => {
+    const {id} = req.params
+    const changes = req.body
+    db.update(id, changes)
+    .then(count => {
+        res.status(200).json(count)
+    })
+    .catch(err => {
+        res.status(500).json({messsage: 'error updating the user'})
+    })
+})
+    
+server.get('/users', (req, res) => {
+ const {id} = req.query;
+
+ if(id) {
+    db.findById(id).then(users => res.send(users))
+ } else {
+     db.find().then(users => res.send(users))
+ }
+
+})
+
+
+
+
+server.listen(9000, () => console.log('/n ==the server is alive!== /n'))
 
 // http://localhost:9000/greet/carlos > {hello: carlos}
 // http://localhost:9000/greet/kevin > {hello: kevin}
